57abd97b2b45ba018059a33dc05c6a3e1924ff47
WhySoFast <matheus.carvalho@outlook.com>
2020-10-17 03:07:55 -0300

    Day 4 - Create map page, import useful dependencies and expo

---
+++ mobile/src/pages/OrphanageDetails.tsx
@@ -0,0 +1,7 @@
+import { DefaultTheme } from "@react-navigation/native";
+import React from "react";
+import { View } from "react-native";
+
+export default function OrphanageDetails() {
+  return <View></View>;
+}
